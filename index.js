32a8ce0e563242447b527d24d3615e82f96d07cd
Boris Strinzha <bs@clickky.biz>
2016-11-08 00:03:15 +0200

    add check digits

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ import express from 'express';
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 app.get('/task2B', (req, res) => {
   const fullName = req.query.fullname;
+  const reDigits = /\d+/g;
   const re = /([^\x00-\x7F]|\w)+/g;
-  const matches = fullName ? fullName.match(re) : null;
+  let matches;
+  if (!reDigits.test(fullName)) {
+    matches = fullName ? fullName.match(re) : null;
+  } else {
+    matches = null;
+  }
+
   let formattedName = '';
   if (matches && matches.length < 4) {
     formattedName += `${matches[matches.length - 1]} `;
